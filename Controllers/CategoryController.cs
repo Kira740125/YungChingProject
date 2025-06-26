c3b87b64770c2be9121bd480aa9f8d9d8c56e67f
Kira Huang <IM0146@imedtac.com>
2025-06-26 16:29:44 +0800

    feat: 新增Category Controller 及 取得Category List

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YungChingProject.Data;
+using YungChingProject.Models;
+
+namespace YungChingProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categories = _db.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
